61ae1cd9cdbabdd71bf07bf46c29afa9afe1dcff
Jim Schmid <jim.schmid@1up.io>
2015-05-28 11:33:25 +0200

    Added basic configuration nodes (mapping)

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -12,6 +12,38 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('oneup_uploader');
 
+        $rootNode
+            ->children()
+                ->arrayNode('mappings')
+                    ->useAttributeAsKey('id')
+                    ->isRequired()
+                    ->requiresAtLeastOneElement()
+                    ->prototype('array')
+                        ->children()
+
+                            // Which frontend uploader will be used.
+                            // This determines the controller linked to the dedicated route
+                            ->enumNode('frontend')
+                                ->values(['dropzone', 'custom'])
+                                ->isRequired()
+                            ->end()
+
+                            // Configures the storage used for this mapping.
+                            ->arrayNode('storage')
+                                ->addDefaultsIfNotSet()
+                                ->children()
+                                    ->enumNode('type')
+                                        ->values(['flysystem', 'gaufrette'])
+                                        ->isRequired()
+                                    ->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
         return $treeBuilder;
     }
 }
